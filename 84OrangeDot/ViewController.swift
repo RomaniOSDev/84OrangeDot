b2978f0d4a991323856051beac7d0f2380685f0d
Roman <romazan.26@gmail.com>
2026-02-07 17:52:43 +0300

    Initial Commit

---
+++ 84OrangeDot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  84OrangeDot
+//
+//  Created by Роман Главацкий on 07.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
